d292a1bbbc06b21506758b462bb280f94f8ddf81
julio-nt <julio.nt753@gmail.com>
2024-01-09 10:48:24 -0300

    added more customization options using props

--- src/Components/CheckBox.tsx
+++ src/Components/CheckBox.tsx
@@ -1,25 +1,29 @@
-import React from "react";
-import { TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export type CheckBoxProps = {
   checked?: boolean;
   color?: string;
   disabled?: boolean;
+  borderRadius?: number;
+  borderWidth?: number;
   onPress?: () => void;
 };
 
 export function CheckBox({
   checked = false,
-  color = "#000",
+  color = '#000',
   disabled = false,
+  borderRadius = 1,
+  borderWidth = 2,
   onPress = () => {},
 }: CheckBoxProps) {
   let backgroundColor,
-    checkColor = "#0000";
+    checkColor = '#0000';
   if (checked) {
     backgroundColor = color;
-    checkColor = "#fff";
+    checkColor = '#fff';
   }
   let opacity = 1;
   if (disabled) {
@@ -32,8 +36,8 @@ export function CheckBox({
       style={{
         backgroundColor,
         borderColor: color,
-        borderRadius: 1,
-        borderWidth: 2,
+        borderRadius,
+        borderWidth,
         opacity,
       }}
     >
